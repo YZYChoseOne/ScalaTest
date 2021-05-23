f0741866fefbb9fc24cfaf5e30b56ce44ca6c362
YzYchoseone <956724366@qq.com>
2021-05-23 22:26:14 +0800

    branch commit 1

--- src/main/scala/test1.scala
+++ src/main/scala/test1.scala
@@ -1,5 +1,11 @@
 object test1 {
   def main(args: Array[String]): Unit = {
     println("git test")
+
+    val array1 = Array(1,2,3)
+    val array2 = Array(77,44,22)
+    val max1: Int = array1.maxBy(x=>x)
+    println(max1)
+
   }
 }
